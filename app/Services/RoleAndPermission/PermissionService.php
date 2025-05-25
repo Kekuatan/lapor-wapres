69e6ea5ba66cb45ee1dbe14f461499f0b62ea62e
Kekuatan <saya.dean@gmail.com>
2025-05-25 18:24:54 +0700

    Clean code PermissionService.php

--- app/Services/RoleAndPermission/PermissionService.php
+++ app/Services/RoleAndPermission/PermissionService.php
@@ -7,11 +7,6 @@ use Illuminate\Support\Facades\Auth;
 
 class PermissionService
 {
-    public function get(){}
-    public function store(array $data){}
-    public function update(string $id, array $data){}
-    public function find($id){}
-
     public function isHasPermission($permission,  $crudKey){
         return !blank(Auth::user()->getPermissionsViaRoles()
             ->where('name', $crudKey . $permission));
